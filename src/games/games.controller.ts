c24e8c11f35a118f60a349d1d539ba5ac9b227d7
Dan <d.jeong@myskolae.fr>
2025-11-13 16:59:27 +0100

    Ajout du Put qui permet de mettre à jour un jeu

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,49 +1,44 @@
 import { GamesService } from './games.service';
 import {
-  Controller,
-  Get,
-  Put,
-  Post,
-  Delete,
-  Param,
-  Body,
-  NotFoundException,
+    Controller,
+    Get,
+    Put,
+    Post,
+    Delete,
+    Param,
+    Body,
+    NotFoundException,
 } from '@nestjs/common';
 
 @Controller('games')
 export class GamesController {
-  constructor(private readonly gamesService: GamesService) {}
-
-  @Get()
-  getAllGames() {
-    return this.gamesService.findAll();
-  }
-
-  @Get(':id')
-  getGamesById(@Param('id') id: string) {
-    const result = this.gamesService.findOne(id);
-
-    if (result) {
-      return result;
-    } else {
-      throw new NotFoundException('Game not found');
+    constructor(private readonly gamesService: GamesService) {}
+    @Get()
+    getAllGames() {
+        return this.gamesService.findAll();
     }
-  }
+    @Get(':id')
+    getGamesById(@Param('id') id: string) {
+        const result = this.gamesService.findOne(Number(id));
 
-  @Post()
-  create(@Body() data: any) {
-    return this.gamesService.addGame(data);
-  }
-
-  @Put(':id')
-  updateGames(@Param('id') id: string, @Body() updateData: any) {
-    // à implémenter plus tard
-    return `mise a jour d'un jeu ${id}`;
-  }
-
-  @Delete(':id')
-  deleteGames(@Param('id') id: string) {
-    // à implémenter plus tard
-    return `suppression d'un jeu ${id}`;
-  }
+        if (result) {
+        return result;
+        } else {
+        throw new NotFoundException('Game not found');
+        }
+    }
+    @Post()
+    create(@Body() data: any) {
+        return this.gamesService.addGame(data);
+    }
+    @Put(':id')
+    updateGames(@Param('id') id: string, @Body() updateBoardgameDto: any) {
+        const result = this.gamesService.modifyGame(Number(id), updateBoardgameDto);
+        return result;
+    }
+    @Delete(':id')
+    deleteGames(@Param('id') id: string) {
+        this.gamesService.deleteGame(Number(id));
+        return 'game deleted';
+    }
 }
--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -4,33 +4,61 @@ import { Game } from './games.entities';
 
 @Injectable()
 export class GamesService {
-  games: Game[] = [];
+    games: Game[] = [];
 
-  constructor() {
-    this.games = bgg as Game[];
-    console.log('First game:', this.games[0]);
-  }
-
-  findAll(): Game[] {
-    return this.games;
-  }
-
-  findOne(id: string): Game | undefined {
-    return this.games.find((game) => game.id === Number(id));
-  }
-
-  addGame(data: any): Game {
-    const game = new Game({
-      id: this.games.length + 1,
-      name: data.name,
-      published_at: data.published_at,
-      min_players: data.min_players,
-      max_players: data.max_players,
-      duration: data.duration,
-      age_min: data.age_min,
-    });
-
-    this.games.push(game);
-    return game;
-  }
+    constructor() {
+        this.games = bgg as Game[];
+        console.log('First game:', this.games[0]);
+    }
+    findAll() {
+        return this.games;
+    }
+    findOne(id: number): Game | undefined {
+        return this.games.find((game) => game.id === Number(id));
+    }
+    addGame(data: any): Game {
+        const game = new Game({
+        id: this.games.length + 1,
+        name: data.name,
+        published_at: data.published_at,
+        min_players: data.min_players,
+        max_players: data.max_players,
+        duration: data.duration,
+        age_min: data.age_min,
+        });
+        this.games.push(game);
+        return game;
+    }
+    modifyGame(id: number, data: any): Game {
+        const game = this.findOne(id);
+        if (!game) {
+        throw new Error('Game not found');
+        }
+        if (data.name) {
+        game.name = data.name;
+        }
+        if (data.published_at) {
+        game.published_at = data.published_at;
+        }
+        if (data.min_players) {
+        game.min_players = data.min_players;
+        }
+        if (data.max_players) {
+        game.max_players = data.max_players;
+        }
+        if (data.duration) {
+        game.duration = data.duration;
+        }
+        if (data.age_min) {
+        game.age_min = data.age_min;
+        }
+        const index = this.games.findIndex((g) => g.id === id);
+        if (index !== -1) {
+        this.games[index] = game;
+        }
+        return game;
+    }
+    deleteGame(id: number): void {
+        this.games = this.games.filter((g) => g.id !== id);
+    }
 }
